f201338f1f187d94defd6d037db3244f9459985a
Duarte Serrão <duarte.serrao.1999@gmail.com>
2023-01-17 22:45:49 +0000

    game com odds

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -4,18 +4,7 @@ import React from "react";
 import { getBookmakers, getOutcomes } from "../api/api";
 import { queryClient } from "../query";
 
-export default function GameInfo({ game}) {
-    const user = queryClient.getQueryData(["user"]);
-
-    const { home_team, away_team } = game;
-    const { data: bookmaker } = useQuery({
-        queryKey: ["bookmaker", game.id],
-        queryFn: () => getBookmakers({ game: game.id }),
-        onSuccess: (data) =>
-            queryClient.setQueryData(["bookmaker", game.id], data.results),
-        onError: console.log,
-    });
-
+const Outcomes = ({bookmaker}) => {
     const { data: outcomes } = useQuery({
         queryKey: ["outcomes", bookmaker?.id],
         queryFn: () => getOutcomes({ bookmaker: bookmaker.id }),
@@ -26,25 +15,42 @@ export default function GameInfo({ game}) {
     });
 
     return (
-        <VStack padding="4" backgroundColor="white" rounded="lg">
-            {outcomes && outcomes.length && (
-                <Flex w="full">
-                    {outcomes.map(({ id, multiplier, result }) => (
-                        <Flex justifyContent={"center"} key={id} flex="1">
-                            <Button
-            
-                                colorScheme="teal"
+        outcomes && outcomes.length && (
+            <Flex w="full">
+                {outcomes.map(({ id, multiplier, result }) => (
+                    <Flex justifyContent={"center"} key={id} flex="1">
+                        <Button colorScheme="teal">
+                            <HStack spacing={5}>
+                                <div>{result}</div>
+                                <div>{multiplier}</div>
+                            </HStack>
+                        </Button>
+                    </Flex>
+                ))}
+            </Flex>
+        )
+    )
+}
 
-                            >
-                                <HStack spacing={5}>
-                                    <div>{result}</div>
-                                    <div>{multiplier}</div>
-                                </HStack>
-                            </Button>
-                        </Flex>
-                    ))}
-                </Flex>
-            )}
+export default function GameInfo({ game}) {
+    const user = queryClient.getQueryData(["user"]);
+
+    const { home_team, away_team } = game;
+    const { data: bookmakers } = useQuery({
+        queryKey: ["bookmaker", game.id],
+        queryFn: () => getBookmakers({ game: game.id, key:"" }),
+        onSuccess: (data) =>
+            queryClient.setQueryData(["bookmaker", game.id], data.results),
+        onError: console.log,
+    });
+
+   
+
+    return (
+        <VStack padding="4" backgroundColor="white" rounded="lg">
+            {bookmakers?.length > 0 && bookmakers.map(({ id, multiplier, result }) => (
+                <Outcomes bookmaker={{id}}/>
+            ))}
         </VStack>
     );
 }
